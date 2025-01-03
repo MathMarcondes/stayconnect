9a581fd7c1dacfa2c8d351581f394993790558cc
mathmarcondes <matheus.marcondes@outlook.com>
2025-01-03 14:05:31 -0300

    Inserção do Body

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
+import Body from "./Body";
 import Header from "./Header";
-import Icon from "./Icons";
+
 import { Text, View, StyleSheet, StatusBar, FlatList, TouchableOpacity, ScrollView } from "react-native";
+
+import Icons from "./Icons";
 
 
 
 export default function Main() {
     return(
       <View>
-      <Header/>
-      <Icon/>
-      
+        <Header/>
+        <Icons/>
+        <Body/>
       </View>
     )
 }
