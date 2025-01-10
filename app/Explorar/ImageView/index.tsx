033c57f5b7ebecedf20f6d6ba7bbea8046ae3cee
mathmarcondes <matheus.marcondes@outlook.com>
2025-01-10 15:27:20 -0300

    Imagepng

--- app/Explorar/ImageView/index.tsx
+++ app/Explorar/ImageView/index.tsx
@@ -3,6 +3,7 @@ import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context"
 import { Text, ScrollView, StyleSheet, Image, View} from "react-native"
 
 const Imagpng = require("../Pikachu.png");
+const imagpng2 = require("../charmander.png");
 
 
 export default function ImageView() {
@@ -10,19 +11,56 @@ export default function ImageView() {
     <SafeAreaProvider>
     <SafeAreaView>
       <ScrollView>
-        <Text style={styles.title}>
-        A GPU RTX sédrie 50 alcança uma fidelidade gráfica superior graças a 
-        sua alta potência de IA, podendo até gerar imagens em alta velocidade 
-        com o NVIDIA Studio. A cereja do bolo, claro, é a melhora de desempenho 
-        com o DLSS 4.
-        Graças ao Hawk Fan, a turbulência e nível de ruído também foram muito 
-        reduzidos enquanto o fluxo de ar aumentou em 12,5%, com a pressão do ar 
-        melhorada em 53,6%. Há uma melhora drástica nas condições de 
-        resfriamento, seguindo a tendência e trabalho dos últimos anos.
-        </Text>
 
-       <Image source={Imagpng} style={{width:300, height:300}}/>
-        
+       <Image source={Imagpng} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
+
+       <Image source={imagpng2} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
+
+       <Image source={Imagpng} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
+
+       <Image source={imagpng2} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
+
+       <Image source={Imagpng} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
+
+       <Image source={imagpng2} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
+
+       <Image source={Imagpng} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
+
+       <Image source={imagpng2} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
+
+       <Image source={Imagpng} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
+
+       <Image source={imagpng2} style={styles.image}/>
+       <Text style={styles.title}>
+       When several of these Pokémon gather, their electricity can build and cause lightning storms.
+       </Text>
       </ScrollView>
     </SafeAreaView>
   </SafeAreaProvider>
@@ -37,8 +75,15 @@ const styles = StyleSheet.create({
         top: 0.5
     },
     title:{
-        fontSize: 40,
+        fontSize: 25,
         padding: 12
+    },
+    image:{
+      width: 150,
+      height: 150,
+      alignItems: 'center',
+      alignContent: 'center',
+      alignSelf: 'center'
     }
  
 })
